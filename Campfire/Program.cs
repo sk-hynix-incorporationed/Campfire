875c6527b0636ec5463284a8b4a37b4f71cf4e78
kraeb <kraeb@kraeb.cc>
2025-08-16 14:57:49 +0200

    meow

--- Campfire/Program.cs
+++ Campfire/Program.cs
@@ -1,22 +1,35 @@
 ﻿using System;
-using System.Net;
-using System.Net.Sockets;
-using System.Threading;
-using System.Collections.Generic;
-using System.Net.Sockets;
+
 namespace Campfire
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            if (args.Length == 0) { Console.WriteLine("Usage: campfire [server|client] [port]"); }
-
-            if (int.TryParse(args[1], out int port));
-            Console.WriteLine("");
-            if (args[0] == "server") Server.Start(port);
-            if (args[0] == "client") Client.Start(); 
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine("  campfire server <port>");
+                Console.WriteLine("  campfire client");
+                return;
+            }
+            if (args[0] == "server")
+            {
+                if (args.Length < 2 || !int.TryParse(args[1], out int port))
+                {
+                    Console.WriteLine("You must provide a valid port for the server.");
+                    return;
+                }
+                Server.Start(port);
+            }
+            else if (args[0] == "client")
+            {
+                Client.Start();
+            }
+            else
+            {
+                Console.WriteLine("Unknown mode. Use 'server' or 'client'.");
+            }
         }
-        
     }
 }
