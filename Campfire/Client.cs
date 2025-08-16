3586cfcb630b9156751803bb5afe56ec8d658982
kraeb <kraeb@localhost.localdomain>
2025-08-16 14:54:24 +0200

    meow

--- Campfire/Client.cs
+++ Campfire/Client.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace Campfire
 {
@@ -11,18 +13,73 @@ namespace Campfire
     {
         public static void Start()
         {
-            Console.Write("Enter fire IP: "); string ip = Console.ReadLine();
-            Console.WriteLine("Enter name"); string name = Console.ReadLine();
+            Console.Write("Enter fire IP: "); 
+            string ip = Console.ReadLine();
+            Console.WriteLine("Enter name"); 
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter fire password"); 
+            string password = Console.ReadLine();
+
+            byte[] Key = SHA256.HashData(Encoding.UTF8.GetBytes(password));
+
             TcpClient client = new TcpClient(ip, 32865);
             NetworkStream stream = client.GetStream();
 
-            new Thread(() => { byte[] buffer = new byte[1024]; try { while (true) { int len = stream.Read(buffer, 0, buffer.Length); if (len == 0) break; Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, len)); } } catch { } } ).Start();
+            new Thread(() => 
+            { 
+                byte[] buffer = new byte[4096]; 
+                try 
+                { 
+                    while (true) 
+                    { 
+                        int len = stream.Read(buffer, 0, buffer.Length); 
+                        if (len == 0) break;
+
+                        string decrypted = Decrypt(buffer.Take(len).ToArray(), Key);
+                        Console.WriteLine(decrypted);
+                    } 
+                } 
+                catch { } 
+            }).Start();
+
             while (true)
             {
                 string line = Console.ReadLine();
                 string mesg = $"{name}: {line}";
-                byte[] data = Encoding.UTF8.GetBytes(mesg);
-                stream.Write(data, 0, data.Length);
+                byte[] encrypted = Encrypt(mesg, Key);
+                stream.Write(encrypted, 0, encrypted.Length);
+            }
+        }
+
+        private static byte[] Encrypt(string plainText, byte[] key)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.GenerateIV();
+                using var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                using var ms = new MemoryStream();
+                ms.Write(aes.IV, 0, aes.IV.Length);
+                using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                using (var sw = new StreamWriter(cs))
+                    sw.Write(plainText);
+                return ms.ToArray();
+            }
+        }
+
+        private static string Decrypt(byte[] cipherData, byte[] key)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                byte[] iv = cipherData.Take(16).ToArray();
+                byte[] ciphertext = cipherData.Skip(16).ToArray();
+                aes.IV = iv;
+                using var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                using var ms = new MemoryStream(ciphertext);
+                using var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
+                using var sr = new StreamReader(cs);
+                return sr.ReadToEnd();
             }
         }
     }
--- Campfire/Server.cs
+++ Campfire/Server.cs
@@ -1,21 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Campfire
 {
     internal class Server
     {
         static List<TcpClient> clients = new List<TcpClient>();
+        static readonly byte[] Key = Encoding.UTF8.GetBytes("ThisIs32BytesLongForAES256Key!!!");
+
         public static void Start(int port)
         {
             TcpListener listener = new TcpListener(IPAddress.Any, 32865);
             listener.Start();
-            Console.WriteLine("Campfire host running on port 38265");
+            Console.WriteLine("Campfire host running on port 32865");
+
             while (true)
             {
                 var client = listener.AcceptTcpClient();
@@ -23,6 +27,7 @@ namespace Campfire
                 new Thread(() => HandleClient(client)).Start();
             }
         }
+
         public static void HandleClient(TcpClient client)
         {
             var stream = client.GetStream();
@@ -35,9 +40,9 @@ namespace Campfire
                     int len = stream.Read(buffer, 0, buffer.Length);
                     if (len == 0) break;
 
-                    string msg = Encoding.UTF8.GetString(buffer, 0, len);
-                    Console.WriteLine(msg);
-                    Broadcast(msg);
+                    string decrypted = Decrypt(buffer, len);
+                    Console.WriteLine(decrypted);
+                    Broadcast(decrypted);
                 }
             }
             catch (Exception ex)
@@ -50,17 +55,47 @@ namespace Campfire
                 client.Close();
             }
         }
+
         public static void Broadcast(string msg)
         {
-            byte[] data = Encoding.UTF8.GetBytes(msg);
+            byte[] encrypted = Encrypt(msg);
             lock (clients)
             {
                 foreach (var c in clients)
                 {
-                    try { c.GetStream().Write(data, 0, data.Length); } catch { }
+                    try { c.GetStream().Write(encrypted, 0, encrypted.Length); } catch { }
                 }
             }
         }
 
+        public static byte[] Encrypt(string plainText)
+        {
+            using var aes = Aes.Create();
+            aes.Key = Key;
+            aes.GenerateIV();
+
+            using var ms = new MemoryStream();
+            ms.Write(aes.IV, 0, aes.IV.Length);
+            using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+            using (var sw = new StreamWriter(cs))
+                sw.Write(plainText);
+
+            return ms.ToArray();
+        }
+
+        public static string Decrypt(byte[] data, int length)
+        {
+            using var aes = Aes.Create();
+            aes.Key = Key;
+
+            byte[] iv = new byte[16];
+            Array.Copy(data, 0, iv, 0, iv.Length);
+            aes.IV = iv;
+
+            using var ms = new MemoryStream(data, iv.Length, length - iv.Length);
+            using var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read);
+            using var sr = new StreamReader(cs);
+            return sr.ReadToEnd();
+        }
     }
 }
